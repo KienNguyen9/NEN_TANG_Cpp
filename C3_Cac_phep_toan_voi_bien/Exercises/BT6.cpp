9c99acacc95eafbed586a201f1403f5a8dc1c8ba
KienNguyen9 <kierannguyen98@gmail.com>
2023-11-14 02:05:17 +0700

    Upload all BT, chưa làm xong bài cuối

--- C3_Cac_phep_toan_voi_bien/Exercises/BT6.cpp
+++ C3_Cac_phep_toan_voi_bien/Exercises/BT6.cpp
@@ -38,13 +38,38 @@ int main()
     if ((x2 >= -1)&& (x2 <= 2) || (x2 >= 4) && (x2<=6))
     {
         check_x1 = true;
-        cout << "x1 thuoc [-1;2] [4;6]";
+        cout << "x2 thuoc [-1;2] [4;6]";
     }  else{
-        cout << "x1 KHONG thuoc [-1;2] [4;6]";
+        cout << "x2 KHONG thuoc [-1;2] [4;6]";
     }
 
     // 3. (-2;2) and 5
+    int x3 = 0;
+    bool check_x3;
+    cout << "\nNhap x3: ";
+    cin >> x3;
+
+    if ((x3 > -2) && (x3 < 2) || (x3 == 5))
+    {
+        check_x1 = true;
+        cout << "x3 thuoc (-2;2) and 5";
+    }  else{
+        cout << "x3 KHONG thuoc (-2;2) and 5";
+    }
+
     // 4. (-4;0] and (4;inf)
+    int x4 = 0;
+    bool check_x4;
+    cout << "\nNhap x4: ";
+    cin >> x4;
+
+    if ((x4 > -4)&& (x4 <= 0) || (x4 >= 4))
+    {
+        check_x1 = true;
+        cout << "x4 thuoc (-4;0] and (4;inf)";
+    }  else{
+        cout << "x4 KHONG thuoc (-4;0] and (4;inf)0";
+    }
 
 
     return 0;
--- C3_Cac_phep_toan_voi_bien/Exercises/BT7.cpp
+++ C3_Cac_phep_toan_voi_bien/Exercises/BT7.cpp
@@ -26,10 +26,60 @@ int main()
     /*
     2. >> 0
     */
-    int x = 0; //0
-    bool b = (x++ == 1); // gáng giá trị 0 cho b rồi mới cộng 1 dv
-    cout << b; // 1
+    // int x = 0; //0
+    // bool b = (x++ == 1); // gáng giá trị 0 cho b rồi mới cộng 1 dv
+    // cout << b; // 1
+    // return 0;
+
+    //3. >> 0
+    // int x = -1; // -1
+    // int y = x*x*abs(x)*2; // 2
+    // cout << y % 2; // 0
+
+    //4. >> 107
+    // int x = 1;
+    // int y = 2;
+    // int z = 3;
+    // int t = 3*(z+(x+y)*(y+z));// 3*(3+15) = 54
+    // cout << 1-2*t; // 1-54*2  = 107 
+
+    //5. >> 1 đúng
+    //    int x = 1;
+    //    int y = 2;
+    //    int z = 3;
+    //    bool b = (x<y)||(x>z)&&((y<z)||(x<z));
+    //    cout << b;
+
+   //6. >> 3 đúng
+    //    int x = 1;
+    //    int y = x++ + 1; // y = 2
+    //    int z = ++x; // z =3
+    //    cout << z;
+    
+    //7. >> 1 đúng
+    // float x = 1.5;
+    // int  y = x; // 1
+    // float z = y; // 1
+    // cout << z; // 1
+
+    //8. >> 1 đúng
+    // int x = 1.5; // 1
+    // float y = x; // 1
+    // cout << (bool)y; // 1
+
+    //9. >> 65 đúng
+//     char c = 'A'; 
+//     float x = c; //65.0 - ép kiểu tự động type casting variable
+//    cout << (int)(char)x;// 65
+    
+    // 10. >> 1
+    char c = 'b';
+    bool b = (c<'c')||!(c>'a') &&(c!='b')&&(c+1>='d');
+    //          q   || !0      &&(0)&& 0
+    cout << b;
     return 0;
+
+
 
 }
     
