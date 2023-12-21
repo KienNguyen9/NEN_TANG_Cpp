a04e0bce4af440087715a45b87f956d9f7d68adb
KienNguyen9 <kierannguyen98@gmail.com>
2023-12-22 02:19:10 +0700

    Upload lý thuyết

---
+++ C10_Phan_vung_nho_STACK/example_code/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file main.cpp
+ * @author Kevin Nguyen
+ * @brief Ví dụ cho vùng nhớ 
+ * @version 0.1
+ * @date 2023-12-22
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int x = 10;
+    int y = 5;
+
+    // Xuất ra màn hình địa chỉ của biến x
+    std::cout << &x;
+
+    return 0;
+}
